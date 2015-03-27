32045ac7bd7db742ee5afec3a75af1e1d2843652
jonnyparris <ruskin.constant+dev@gmail.com>
2015-03-27 14:02:35 +0000

    make sure visitors controller includes model associations

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,8 @@
 class VisitorsController < ApplicationController
 
   def index
-    @venues = Venue.all
-    @vendors = Vendor.all unless Vendor.all.empty?
+    @venues = Venue.includes(:preferred_vendors).all
+    @vendors = Vendor.includes(:preferred_venues).all unless Vendor.all.empty?
   end
 
 end
