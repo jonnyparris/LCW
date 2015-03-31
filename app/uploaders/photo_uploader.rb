724449c5b0c9146de3de6c186504d3ae64c930dd
jonnyparris <ruskin.constant+dev@gmail.com>
2015-03-31 14:08:56 +0100

    fix thumbnails for better resolution

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -33,10 +33,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  process resize_to_fit: [800, 800]
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [75, 75]
+    process :resize_to_fill => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
