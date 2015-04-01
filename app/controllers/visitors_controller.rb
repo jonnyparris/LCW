069b0e19d0ddd3c6175b5cd5916f7ae28d7f0d18
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-01 16:50:23 +0100

    enable child friendly and disabled access filters

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,7 +4,12 @@ class VisitorsController < ApplicationController
     if params[:filter].nil?
       @venues = Venue.includes(:preferred_vendors).all unless Venue.all.empty?
     else
-      @venues = Venue.includes(:preferred_vendors).where("pricing < ? AND size_details > ?", params[:max_price], params[:min_capacity])
+      @venues = Venue.includes(:preferred_vendors)
+        .where("pricing < ? AND size_details > ? AND children_friendly = ? AND disabled_access = ?",
+                params[:max_price],
+                params[:min_capacity],
+                params[:children_friendly] != nil,
+                params[:disabled_access] != nil)
     end
     @vendors = Vendor.includes(:preferred_venues).all unless Vendor.all.empty?
   end
