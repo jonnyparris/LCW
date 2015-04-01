93549bc0409a36a9924a2fb438f4a9aac749db09
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-01 18:23:37 +0100

    pass updated filter test

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,12 +4,14 @@ class VisitorsController < ApplicationController
     if params[:filter].nil?
       @venues = Venue.includes(:preferred_vendors).all unless Venue.all.empty?
     else
+      children_friendly = params[:children_friendly] ? "true" : "false"
+      disabled_access = params[:disabled_access] ? "true" : "false"
       @venues = Venue.includes(:preferred_vendors)
         .where("pricing < ? AND size_details > ? AND children_friendly = ? AND disabled_access = ?",
                 params[:max_price],
                 params[:min_capacity],
-                params[:children_friendly] != nil,
-                params[:disabled_access] != nil)
+                children_friendly,
+                disabled_access)
     end
     @vendors = Vendor.includes(:preferred_venues).all unless Vendor.all.empty?
   end
