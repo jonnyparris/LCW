a62d4c2ca00c6adafebaa4730a37c96588327cdf
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-01 16:45:27 +0100

    add child friendly and disabled access to venues model

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe Venue, :type => :model do
     it { should respond_to(:contact_details) }
     it { should respond_to(:size_details) }
     it { should respond_to(:photos) }
+    it { should respond_to(:children_friendly) }
+    it { should respond_to(:disabled_access) }
   end
 
   describe "associations" do
