f82fd7cf87a617d4ea7bc7ef074d50827e99634b
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-01 13:14:56 +0100

    pass venue range filters feature test

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,11 @@
 class VisitorsController < ApplicationController
 
   def index
-    @venues = Venue.includes(:preferred_vendors).all
+    if params[:filter].nil?
+      @venues = Venue.includes(:preferred_vendors).all unless Venue.all.empty?
+    else
+      @venues = Venue.includes(:preferred_vendors).where("pricing < ? AND size_details > ?", params[:max_price], params[:min_capacity])
+    end
     @vendors = Vendor.includes(:preferred_venues).all unless Vendor.all.empty?
   end
 
