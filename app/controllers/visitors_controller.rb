0adde5f2c201237fe744fff62cdeb48196f49e15
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-02 08:21:08 +0100

    remove redundant filter input

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,17 +1,20 @@
 class VisitorsController < ApplicationController
 
   def index
-    if params[:filter].nil?
-      @venues = Venue.includes(:preferred_vendors).all unless Venue.all.empty?
-    else
+    if params[:commit] == "Filter"
       children_friendly = params[:children_friendly] ? "true" : "false"
       disabled_access = params[:disabled_access] ? "true" : "false"
       @venues = Venue.includes(:preferred_vendors)
-        .where("pricing < ? AND size_details > ? AND children_friendly = ? AND disabled_access = ?",
-                params[:max_price],
-                params[:min_capacity],
-                children_friendly,
-                disabled_access)
+                      .where("pricing < ?
+                              AND size_details > ?
+                              AND children_friendly = ?
+                              AND disabled_access = ?",
+                              params[:max_price],
+                              params[:min_capacity],
+                              children_friendly,
+                              disabled_access)
+    else
+      @venues = Venue.includes(:preferred_vendors).all unless Venue.all.empty?
     end
     @vendors = Vendor.includes(:preferred_venues).all unless Vendor.all.empty?
   end
