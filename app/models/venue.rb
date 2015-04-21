80adb068b1b88f4a52217d24437e1cb717470bd2
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-21 17:37:23 +0200

    venues can import and export csv files

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -12,4 +12,61 @@ class Venue < ActiveRecord::Base
     return if self.website? && /^http/i.match(self.website)
     self.website = "http://#{self.website}"
   end
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |venue|
+        csv << venue.attributes.values_at(*column_names)
+      end
+    end
+  end
+
+  def self.import(file)
+    raise "No file uploaded" if file.nil?
+    spreadsheet = open_spreadsheet(file)
+    bad_eggs = []
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      venue = find_by_id(row["id"]) || new
+      venue.attributes = row.to_hash.slice(*accessible_attributes)
+      venue.remote_photos_urls = [row["new_photos"]] unless row["new_photos"].nil?
+      if venue.valid? == false
+        bad_eggs << [venue.name, venue.errors.full_messages]
+      else
+        venue.save!
+      end
+    end
+    if bad_eggs.empty?
+      "success"
+    else
+      "some success, some bad eggs: #{bad_eggs.to_s}"
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv" then Roo::Csv.new(file.path, csv_options: { :encoding => 'windows-1251:utf-8' })
+    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+  def self.accessible_attributes
+   ["name",
+    "description",
+    "pricing",
+    "directions",
+    "website,",
+    "contact_details",
+    "size_details",
+    "children_friendly",
+    "disabled_access",
+    "address",
+    "bad_weather_capacity",
+    "deposit",
+    "onsite_accommodation"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :vendors
 
-  resources :venues
+  resources :venues do
+    collection { post :import }
+  end
 
   root to: 'visitors#index'
   devise_for :users
